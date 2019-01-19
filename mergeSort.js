cb033d05573f6ef13ae1770cccb1cff8cfc7d1f3
normanrs <normanrs@live.com>
2019-01-19 15:08:29 -0700

    working and tested

--- mergeSort.js
+++ mergeSort.js
@@ -1,6 +1,20 @@
+pry = require('pryjs');
 
-function mergeSort(array_in) {
-  return array_in
+function mergeSort(arrayIn) {
+  if (arrayIn.length < 2) { return(arrayIn) ;}
+  else if (Array.isArray(arrayIn)) {
+    let left = arrayIn.slice(0,Math.floor(arrayIn.length/2));
+    let right = arrayIn.slice(Math.floor(arrayIn.length/2));
+    return sorted(mergeSort(left), mergeSort(right));
+  }
+  function sorted(half1, half2) {
+    for (let loc = 0; loc < half2.length; loc++) {
+      if (half1[0] < half2[loc]) {
+        half2.splice(loc, 0, half1.shift());
+      }
+    } half2 = [...half2, ...half1];
+    return half2;
+  }
 }
 
 module.exports = mergeSort
--- test/merge_test.js
+++ test/merge_test.js
@@ -1,10 +1,14 @@
+pry = require('pryjs');
+
 const assert = require('chai').assert;
 const merge = require('../mergeSort')
 
 describe('Merge sort', function() {
   it("can sort a random array of munbers", function() {
-    var unsorted = [66364, 70434, 4908, 57113, 2987, 18708, 15341, 93345, 72823, 95803, 25156, 97621, 19954, 97759, 10231, 30912, 60305, 97370, 46693, 2393, 20268, 87402, 65899, 20619, 52765, 8304, 50427, 35445, 51366, 459, 45944, 75838, 1486, 33427, 29936, 91376, 3294, 82857, 92607, 69310, 47204, 88392, 21361, 59024, 42363, 62181, 7915, 23599, 67623, 55397, 44926, 53678, 22426]
-    let sorted = merge(unsorted);
-    assert(true, 'True is false! Up is down!');
+    let randArray = new Array(20).fill(0).map(function(n) {return parseInt(Math.random() * (100 - 1) + 1); });
+    var unsorted = [26, 96, 68, 28, 98, 62, 91, 35, 74, 62, 23, 48, 16, 61, 53, 25, 5, 46, 50, 76]
+    let result = merge(randArray)
+    assert.equal(Math.min(...result), result[0])
+    assert.equal(Math.max(...result), result[result.length - 1])
   });
 });
